41fcd43096698073af7d4a9be36b8293b5f9556f
Murray Kopit <murr2k@gmail.com>
2025-08-02 21:49:06 -0700

    🔖 Checkpoint: Edit /home/murr2k/projects/docker-dev/docker-demo-showcase/pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import type { AppProps } from 'next/app'
-import '@/styles/globals.css'
-import Layout from '@/components/layout/Layout'
+import '../styles/globals.css'
+import '../styles/v2/design-system.css'
+import Layout from '../components/layout/Layout'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const isV2Page = router.pathname !== '/docker-hub' && 
+                   router.pathname !== '/build-cloud' && 
+                   router.pathname !== '/scout' &&
+                   router.pathname !== '/index-v1'
+
+  useEffect(() => {
+    // Add v2 class to body for new pages
+    if (isV2Page) {
+      document.body.classList.add('v2-theme')
+    } else {
+      document.body.classList.remove('v2-theme')
+    }
+  }, [isV2Page])
+
+  // Don't use layout for v2 pages
+  if (isV2Page) {
+    return <Component {...pageProps} />
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
